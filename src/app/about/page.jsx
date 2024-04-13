ba22be0007f8200272ed3b63a8d3f934ec47a08a
anishshetyy6 <anishshetty2708@gmail.com>
2024-04-13 18:42:02 +0530

    homepage done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import styles from "./about.module.css"
 const AboutPage = () => {
     return (
-        <div className="{styles.imgContainer"> 
+        <div className={styles.imgContainer}> 
             <Image src="/about.png" alt="" width={400} height={400}></Image>
         </div>
     )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,23 @@
 
 import styles from "./page.module.css";
-import React from "react";
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>Homepage</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency.</h1>
+        <p>Transform your ideas into reality. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, fugiat dicta? Adipisci laboriosam in possimus!</p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Get Started</button>
+          <button className={styles.button}>Learn More</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} ></Image>
+      </div>
+      </div>
   );
 }
