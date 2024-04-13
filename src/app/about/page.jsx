7ef167b92493afd1f3be93cec61b2d56aa37ce90
anishshetyy6 <anishshetty2708@gmail.com>
2024-04-13 18:52:46 +0530

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,52 @@
-import Image from "next/image"
-import styles from "./about.module.css"
-const AboutPage = () => {
-    return (
-        <div className={styles.imgContainer}> 
-            <Image src="/about.png" alt="" width={400} height={400}></Image>
-        </div>
-    )
-}
+import Image from "next/image";
+import styles from "./about.module.css";
 
-export default AboutPage
+export const metadata = {
+  title: "About Page",
+  description: "About description",
+};
+
+
+const AboutPage = () => {
+
+  // console.log("lets check where it works")
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h3 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h3>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/about.png"
+          alt="About Image"
+          fill
+          className={styles.img}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
